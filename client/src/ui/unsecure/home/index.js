4c3a9be057436ebc6487914494c5abef6841d1b4
Tanner Bell <twbell17@gmail.com>
2018-12-03 19:30:10 -0700

    First commit

---
+++ client/src/ui/unsecure/home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Page, Content } from './styles'
+
+class Homepage extends Component {
+  render() {
+    return (
+      <Page>
+        <Content>
+          Welcome to mywebsite.
+        </Content>
+      </Page>
+    )
+  }
+}
+
+export default Homepage
